03d6a3ddcb448019d50dcf58b37e9dfa843495b7
ksturgeong <ksturgeongcc@gmail.com>
2023-03-09 15:34:49 +0000

    adding more routes

--- pages/blog/details.php
+++ pages/blog/details.php
@@ -146,7 +146,7 @@ $comments->bind_result($cID, $cHeading, $comment, $cDateAdded, $pending, $userna
                     </p>
                 </div>
                 <div class="max-w-2xl mx-auto">
-                    <form action="../../account/dashboard/user/addComment.php?blog_id=<?= $blogID ?>" method="post">
+                    <form action="../account/dashboard/user/addComment.php?blog_id=<?= $blogID ?>" method="post">
                         <input type="hidden" name="fk_user_id" value="<?= $userID ?>">
                         <input type="hidden" name="fk_blog_id" value="<?= $blogID ?>">
                         <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
